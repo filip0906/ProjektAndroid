68dde8bf6746b79bdbf16149e5155999e8b4eeec
Filip Marić <filip0906@gmail.com>
2022-10-18 17:00:51 +0200

    Kraj
    Gotovo

--- app/src/main/java/com/example/todoprojekt/MainActivity.kt
+++ app/src/main/java/com/example/todoprojekt/MainActivity.kt
@@ -2,13 +2,45 @@ package com.example.todoprojekt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todoprojekt.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TaskItemClickListener
+{
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
 
-    private lateinit var binding : ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener {
+            NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
+        }
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView()
+    {
+        val mainActivity = this
+        taskViewModel.taskItems.observe(this){
+            binding.todoListRecyclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = TaskItemAdapter(it, mainActivity)
+            }
+        }
+    }
+
+    override fun editTaskItem(taskItem: TaskItem)
+    {
+        NewTaskSheet(taskItem).show(supportFragmentManager,"newTaskTag")
+    }
+
+    override fun completeTaskItem(taskItem: TaskItem)
+    {
+        taskViewModel.setCompleted(taskItem)
     }
 }
---
+++ app/src/main/java/com/example/todoprojekt/TaskItem.kt
@@ -0,0 +1,23 @@
+package com.example.todoprojekt
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.*
+
+class TaskItem(
+    var name: String,
+    var desc: String,
+    var dueTime: LocalTime?,
+    var completedDate: LocalDate?,
+    var id: UUID = UUID.randomUUID()
+)
+{
+    fun isCompleted() = completedDate != null
+    fun imageResource(): Int = if(isCompleted()) R.drawable.ic_check_24 else R.drawable.ic_unchecked_24
+    fun imageColor(context: Context): Int = if(isCompleted()) purple(context) else black(context)
+
+    private fun purple(context: Context) = ContextCompat.getColor(context, R.color.purple_500)
+    private fun black(context: Context) = ContextCompat.getColor(context, R.color.black)
+}
